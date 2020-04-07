87be13aefbf21c9441c89741316aabf98d3d51d0
Aman Jain <56549638+amanjain4103@users.noreply.github.com>
2020-04-08 03:06:40 +0530

    added new server file and removed old

--- server.js
+++ server.js
@@ -1,207 +1,105 @@
-const express = require('express');
 const puppeteer = require('puppeteer');
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const knex = require('knex');
 
 
 const app = express();
-
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
 
-const db = knex({
-  client: "pg",
-  connection: {
-    host: "localhost",
-    user: "postgres",
-    password: "system",
-    database: "covid19"
-  }
-});
 
-
-
-
-
-
-///////////////////////////////////////////////////
-///      web scrapping starts here              ///
-///////////////////////////////////////////////////
-
-
+var stats=[];
 
 async function getData(){
-  const url = "https://www.mohfw.gov.in/";
-  const browser = await puppeteer.launch({
-    args: [
-      '--incognito',
-    ],
-  });
-  const page = await browser.newPage();
-  await page.goto(url);
+    const url = "https://www.mohfw.gov.in/";
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+     const page = await browser.newPage();
+    await page.goto(url);
+    console.log("opened url");
+    try{
+        for(var i=1;i<50;i++){
+            //s.no selector 
+            var selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(1)`);
+            var parsing = await selector.getProperty('innerText');
+            var x1 = await parsing.jsonValue();
+            let i1 =await parseInt(x1);
+            
+            //checking till the last value
+            if(isNaN(i1)){break}
+            
+            //checking previously and now i can work with i1
+            // console.log(i1)
 
-  //getting cases
-  for(let i=1;i<15;i++){
-
-    let selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(3)`);
-    let parsing = await selector.getProperty('textContent');
-    let data = await parsing.jsonValue();
-    // console.log(parseInt(data));
-    let newData = parseInt(data);
-
-     db("stats").where("id",i)
-    .update({cases:newData}).then((d)=> { });
-
-  }
-
-
-  //getting survived
-  for(let i=1;i<15;i++){
-    
-    let selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(4)`);
-    let parsing = await selector.getProperty('textContent');
-    let data = await parsing.jsonValue();
-    // console.log(parseInt(data));
-    let newData = parseInt(data);
-
-     db("stats").where("id",i)
-    .update({survived:newData}).then((d)=> { });
-  }
+            //fetch name of states 
+            var selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(2)`);
+            var parsing = await selector.getProperty('innerText');
+            var x2 = await parsing.jsonValue();
+            let i2 = x2;
+            // console.log(x2);
 
 
-    //getting deaths 
-  for(let i=1;i<15;i++){
-    
-    let selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(5)`);
-    let parsing = await selector.getProperty('textContent');
-    let data = await parsing.jsonValue();
-    // console.log(parseInt(data));
-    let newData = parseInt(data);
+            //fetch number of cases
+            var selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(3)`);
+            var parsing = await selector.getProperty('innerText');
+            var x3 = await parsing.jsonValue();
+            let i3 =await parseInt(x3);
+            // console.log(i3);
+
+            //fetch number of peoples survived 
+            var selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(4)`);
+            var parsing = await selector.getProperty('innerText');
+            var x4 = await parsing.jsonValue();
+            let i4 =await parseInt(x4);
+            // console.log(i4);
+
+            //fetch number of peoples died
+            var selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(5)`);
+            var parsing = await selector.getProperty('innerText');
+            var x5 = await parsing.jsonValue();
+            let i5 =await parseInt(x5);
+            // console.log(i5);
+
+            let obj = {
+                "id":i1,
+                "name":i2,
+                "cases":i3,
+                "survived":i4,
+                "deaths":i5
+                }
+             stats.push(obj);
+            
+        }
+    }catch(error){
+        console.log(error);
+    }
    
-     db("stats").where("id",i)
-     .update({deaths:newData}).then((d)=> { });
-    
-
-  }
-
-
-  //getting total number of cases
-  var selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(31) > td:nth-child(2) > strong`);
-  var parsing = await selector.getProperty('textContent');
-  var data = await parsing.jsonValue();
-  let newData =await parseInt(data);
-  // console.log(newData);
-
-   db("stats").where("id","30")
-  .update({cases:newData}).then((d)=> { });
-
-  //getting total survived 
-  var selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(31) > td:nth-child(3) > strong`);
-  var parsing = await selector.getProperty('textContent');
-  var data = await parsing.jsonValue();
-  var newData2 =await parseInt(data);
-
-   db("stats").where("id","30")
-  .update({survived:newData2}).then((d)=> { });
-
-
-  //getting total deaths
-  var selector = await page.$(`#state-data > div > div > div > div > table > tbody > tr:nth-child(31) > td:nth-child(4) > strong`);
-  var parsing = await selector.getProperty('textContent');
-  var data = await parsing.jsonValue();
-  var newData3 =await parseInt(data);
-
-   db("stats").where("id","30")
-  .update({deaths:newData3}).then((d)=> { });
-
-
-
-  await browser.close();
+    browser.close();
 }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log("first")
-
-// // db('stats').update({cases:400}).where({id:30});
-
-// console.log("last")
-
-// await db("stats").where("id","30")
-//   .update({cases: 40333}).then((d)=> { });
-
-// console.log("last 2")
-
-
-
-///////////////////////////////////////////////////
-///      Data updated here every 12 hr          ///
-///////////////////////////////////////////////////
-
-
-console.log("hello")
-setInterval(getData,43200000);
-
-
-
-
-///////////////////////////////////////////////////
-///      requests handled below                 ///
-///////////////////////////////////////////////////
-
-
 app.get('/',(req,res)=>{
-  
-  db.select('*').from('stats')
-  .then(data => res.json(data) )
-  .catch(err => res.json("error ocuured"))
+    try{
+        getData();
+        
+    }catch(error){
+        console.log(error)
+    }
 
+    const sendResp = () =>{
+        res.status(200).json(stats);
+    }
+
+    setTimeout(sendResp,15000);
 })
-
-
-///////////////////////////////////////////////////
-///      listening to a port below              ///
-///////////////////////////////////////////////////
 
 
 
 const PORT = process.env.PORT;
-
-app.listen(PORT || 3000,resp=>{
-  console.log("app is running");
-});
-
-
-
-
-
-
-
-
-
+app.listen(PORT || 3000)
 
 
